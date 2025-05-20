3fbd5da6bed466e6ec4708baa83c31c52006d1f8
João Victor Farchi <91800826+notfarchi@users.noreply.github.com>
2025-05-19 21:33:13 -0300

    Delete Documents directory

--- Documents/tic-java-crud/src/main/java/com/consumption/crud/repositories/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.consumption.crud.repositories;
-
-import com.consumption.crud.models.PessoaModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepository extends JpaRepository<PessoaModel, Integer> {
-}
